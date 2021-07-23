3c7915514882e711a010314d07f23bb7f39b4f37
Shaheer Ahmed <shaheerahmed@Shaheers-MacBook-Pro.local>
2021-07-23 12:46:13 -0400

    Setup DB with App

--- beats-site/client/src/App.js
+++ beats-site/client/src/App.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navi from "./components/Navi/Navi";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [allBeats, setAllBeats] = useState([]);
+
+  //Variables used to manually input music to DB
+  /*const [filename, setfilename] = useState("Faith");
+  const [tempo, setTempo] = useState(147);
+  const [key, setKey] = useState('E');
+  const [scale, setScale] = useState('Minor');
+  const [tempFile, setTempFile] = useState('https://drive.google.com/file/d/1LXvkMW8OzqHEjIHvUoRCvJl0f8Cw8kPz/view?usp=sharing')
+*/
+  useEffect(() => {
+
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setAllBeats(response.data);
+    })
+
+  }, []);
+
+  /*useEffect(() => {
+
+    Axios.post("http://localhost:3001/api/upload", {
+      Name: filename,
+      Tempo: parseInt(tempo),
+      Key: key,
+      Scale: scale,
+      tempFile: tempFile,
+    });
+
+  }, [filename, tempo, key, scale, tempFile]);*/
+
   return (
     <div className="App">
-    <Navi></Navi>
+    <Navi />
+
+    {allBeats.map((val) => {
+      
+      return (
+        <audio key={val.Name} controls>
+          <source src={val.musicFile} type="audio/mpeg"></source>
+          <source src={"https://drive.google.com/file/d/1LXvkMW8OzqHEjIHvUoRCvJl0f8Cw8kPz/view?usp=sharing"} type="audio/wav"></source>
+          <source src={"https://drive.google.com/file/d/1LXvkMW8OzqHEjIHvUoRCvJl0f8Cw8kPz/view?usp=sharing"} type="audio/ogg"></source>
+          <p>Wont work</p>
+        </audio>
+      )
+    })}
     </div>
   );
 }
--- beats-site/server/index.js
+++ beats-site/server/index.js
@@ -3,10 +3,41 @@ const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password123',
+    database: 'beatsDB',
+})
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(5000, () => {
-    console.log("running on 5000");
+app.get('/api/get', (req, res) => {
+    const getBeats = "SELECT * FROM beatsLinks";
+    db.query(getBeats, (err, result) => {
+        res.send(result);
+    })
+})
+
+//This is used to manually uplaod music link and info to DB
+/*app.post('/api/upload', (req, res) => {
+
+    const Name = req.body.Name;
+    const Tempo = req.body.Tempo;
+    const Key = req.body.Key;
+    const Scale = req.body.Scale;
+    const musicFile = req.body.tempFile;
+
+    const sqlUpload = "INSERT INTO beatsLinks VALUES (?, ?, ?, ?, ?);";
+    db.query(sqlUpload, [Name, Tempo, Key, Scale, musicFile], (err, result) => {
+    })
+})
+*/
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`running on ${PORT}`);
 });
