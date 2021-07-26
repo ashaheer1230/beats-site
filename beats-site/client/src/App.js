7f3800c17dcccf48968f158e21565d762558e481
Shaheer Ahmed <shaheerahmed@Shaheers-MacBook-Pro.local>
2021-07-26 16:55:37 -0400

    Progress made with front page GUI + functionality

--- beats-site/client/src/App.js
+++ beats-site/client/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Navi from "./components/Navi/Navi";
 import './App.css';
 import Axios from 'axios';
+import musicImg from './musicImg.jpg';
 
 function App() {
 
@@ -36,18 +37,45 @@ function App() {
 
   return (
     <div className="App">
-    <Navi />
+      <div>
+        < Navi />
+      </div>
+      <div className="row">
 
-    {allBeats.map((val) => {
+        <div className="column">
+          <div className="filterSection">
+            <h1>Test</h1>
+          </div>
+        </div>
+
+        <div className="column">
+          {allBeats.map((val) => {
+          
+            return (
+              <div className="beatCard">
+
+                <div flex={1}>
+                  <img src={musicImg} alt="Cant display" className="cardImg">
+                  </img>
+                </div>
+
+                <div flex={1}>
+                  <h2 className="cardText">{val.Name}</h2>
+                  <h5 className="cardText">{val.Tempo}bpm</h5>
+                  <h5 className="cardText">{val.Key} {val.Scale}</h5>
+                  <audio key={val.Name} controls controlsList="nodownload">
+                    <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/mpeg"></source>
+                    <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/wav"></source>
+                    <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/ogg"></source>
+                    <p>Your browser does not support audio playback</p>
+                  </audio>
+                </div>
+                
+              </div>
+          )})}
+        </div>
+      </div>
       
-      return (
-        <audio key={val.Name} controls>
-          <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/mpeg"></source>
-          <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/wav"></source>
-          <source src={"https://docs.google.com/uc?export=download&id=" + val.musicFile} type="audio/ogg"></source>
-          <p>Your browser does not support audio playback</p>
-        </audio>
-   )})}
     </div>
   );
 }
