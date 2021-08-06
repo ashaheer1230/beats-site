c1cf0f8c2739878cb660f86d9f34113d9d092e8b
Shaheer Ahmed <shaheerahmed@Shaheers-MacBook-Pro.local>
2021-08-05 23:20:57 -0400

    newTorontoMusic

--- beats-site/client/src/App.js
+++ beats-site/client/src/App.js
@@ -3,6 +3,7 @@ import Navi from "./components/Navi/Navi";
 import './App.css';
 import Axios from 'axios';
 import musicImg from './musicImg.jpg';
+import { Button } from './Button.js'
 
 //const Spacer = require('react-spacer');
 
@@ -16,7 +17,6 @@ function App() {
   const [key, setKey] = useState('E');
   const [scale, setScale] = useState('Minor');
   const [tempFile, setTempFile] = useState('1KCYiJMSK4TW8X8-1jmp6nu8OkAzwzoq_');*/
-
   useEffect(() => {
 
     Axios.get("http://localhost:3001/api/get").then((response) => {
@@ -39,6 +39,7 @@ function App() {
 
   return (
     <div className="App">
+      
       <div>
         < Navi />
       </div>
@@ -47,71 +48,25 @@ function App() {
 
         <div className="column-left">
           <div className="filterSection">
-            <h1>Filters </h1>
-            <br/>
+            <br></br>
 
-            <form>
-              <h3>Tempo</h3>
-              <input type="checkbox" id="less90" name="less90" value="90"/>
-              <label for="less90"> -90</label><br/>
-              <input type="checkbox" id="90109" name="90109" value="90109"/>
-              <label for="90109">90 - 109</label><br/>
-              <input type="checkbox" id="110129" name="110129" value="110129"/>
-              <label for="110129">110 - 129</label><br/>
-              <input type="checkbox" id="130149" name="130149" value="130149"/>
-              <label for="130149">130 - 149</label><br/>
-              <input type="checkbox" id="150165" name="150165" value="150165"/>
-              <label for="150165">150 - 165</label><br/>
-              <input type="checkbox" id="more165" name="more165" value="165"/>
-              <label for="more165"> 165+ </label><br/>
+            <h1>newTorontoMusic</h1>
 
-              <br></br>
+            <br></br>
+            <br></br>
 
-              <h3>Scale</h3>
-              <input type="checkbox" id="major" name="major" value="major"/>
-              <label for="major">Major</label><br/>
-              <input type="checkbox" id="minor" name="minor" value="minor"/>
-              <label for="minor">Minor</label><br/>
-              <input type="checkbox" id="other" name="other" value="other"/>
-              <label for="other">Other</label><br/>
+            <h4>newTorontoMusic is a production group aiming to shape the sound of the next generation of Toronto music.</h4>
 
-              <br></br>
+            <br></br>
 
-              <h3>Key</h3>
-              
-              <input type="checkbox" id="A" name="A" value="A"/>
-              <label for="A">A</label><br/>
-              <input type="checkbox" id="A#" name="A#" value="A#"/>
-              <label for="A#">A#</label><br/>
-              <input type="checkbox" id="B" name="B" value="B"/>
-              <label for="B">B</label><br/>
-              <input type="checkbox" id="C" name="C" value="C"/>
-              <label for="C">C</label><br/>
-              <input type="checkbox" id="C#" name="C#" value="C#"/>
-              <label for="C#">C#</label><br/>
-              <input type="checkbox" id="D" name="D" value="D"/>
-              <label for="D">D</label><br/>
-              <input type="checkbox" id="D#" name="D#" value="D#"/>
-              <label for="D#">D#</label><br/>
-              <input type="checkbox" id="E" name="E" value="E"/>
-              <label for="E">E</label><br/>
-              <input type="checkbox" id="F" name="F" value="F"/>
-              <label for="F">F</label><br/>
-              <input type="checkbox" id="F#" name="F#" value="F#"/>
-              <label for="F#">F#</label><br/>
-              <input type="checkbox" id="G" name="G" value="G"/>
-              <label for="G">G</label><br/>
-              <input type="checkbox" id="G#" name="G#" value="G#"/>
-              <label for="G#">G#</label><br/>
+            <h2>Producers: </h2>
+            <br></br>
+            <h4> (Coming Soon) </h4>
 
-              
-              <br/>
-              <br/>
+          </div>
 
-              <input type="button" id="submit" value="submit"></input>
+          <div className="emptySpace">
 
-
-            </form>
           </div>
         </div>
 
